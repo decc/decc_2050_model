65b7926fb6279a417df4531e189930bcc592bf18
greg3000 <greg@ubuntu.(none)>
2012-09-11 10:47:15 +0100

    Updated generated C and ruby files to model version 3.4.0

--- util/start-high-memory-instance.sh
+++ util/start-high-memory-instance.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 ami_id=ami-1de8d369 # Ubuntu 12.04 'Precise' server
-ec2-run-instances --private-key ~/.ssh/private-key-david-spike-aws.pem --cert ~/.ssh/public-certificate-david-spike-aws.pem -k ds-ec2 --instance-type m2.xlarge  $ami_id
+ec2-run-instances --region eu-west-1 --private-key ~/.ssh/private-key-david-spike-aws.pem --cert ~/.ssh/public-certificate-david-spike-aws.pem -k ds-ec2 --instance-type m2.xlarge  $ami_id
 sleep 10
-ec2-describe-instances  --private-key ~/.ssh/private-key-david-spike-aws.pem --cert ~/.ssh/public-certificate-david-spike-aws.pem
+ec2-describe-instances  --region eu-west-1 --private-key ~/.ssh/private-key-david-spike-aws.pem --cert ~/.ssh/public-certificate-david-spike-aws.pem
+
 
