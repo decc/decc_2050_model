ebbfc0a6c0b1227df15564dec6b10a5066b6ccd8
Tom Counsell <tom@counsell.org>
2013-12-24 20:31:03 +0000

    Refactor the test_model_result utility script

--- test/test_model_result.rb
+++ test/test_model_result.rb
@@ -1,31 +1,39 @@
 # coding: utf-8
 # Test for model_result
+require 'rubygems'
 gem 'minitest'
-require 'test/unit'
+require 'minitest/autorun'
+
 require_relative '../lib/model'
 require 'pp'
 
-class TestModelResult < Test::Unit::Test
+class TestModelResult < MiniTest::Test
 
-  def test_results
-    @model = ModelResult.new
-    tests_folder = File.join(File.dirname(__FILE__),"expected_results")
-    Dir[File.join(tests_folder, "*.json")].each do |test|
-      expected = IO.readlines(test)
-      pathway = expected.shift.strip
-      expected = expected.join
-      puts "Testing #{pathway}"
-      @model.reset
-      result = @model.calculate_pathway(pathway).pretty_inspect
-      if result != expected
-        File.open(test+".actual",'w') do |f|
-          f.puts pathway
-          f.puts result
-        end
-      end
-      assert_equal expected, result
-    end
+  def model
+    @model ||= ModelResult.new
   end
+
+    tests_folder = File.join(File.dirname(__FILE__),"expected_results")
+
+    Dir[File.join(tests_folder, "*.json")].each do |test|
+
+      define_method "test_#{test}" do
+        expected = IO.readlines(test)
+        pathway = expected.shift.strip
+        expected = expected.join
+        puts "Testing #{pathway}"
+        model.reset
+        result = model.calculate_pathway(pathway).pretty_inspect
+        if result != expected
+          File.open(test+".actual",'w') do |f|
+            f.puts pathway
+            f.puts result
+          end
+        end
+        assert_equal expected, result
+      end
+    end
 end
+puts "ok"
 
     
